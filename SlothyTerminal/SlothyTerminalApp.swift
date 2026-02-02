3df83c5fc8121909fa47a2bf062726d2eb4f80a8
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-02-02 16:29:01 +0500

    Initial Commit

---
+++ SlothyTerminal/SlothyTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlothyTerminalApp.swift
+//  SlothyTerminal
+//
+//  Created by Maxim Gorbatyuk on 02.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SlothyTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
